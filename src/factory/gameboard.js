d5437479df9060d45e99c628db726524cee594ee
Ken Yamada <yamakenth@gmail.com>
2021-12-26 19:26:29 +0700

    Add function to place ship

--- src/factory/gameboard.js
+++ src/factory/gameboard.js
@@ -2,17 +2,32 @@
 class Gameboard {
   // 10x10 board 
   constructor() {
-    this.board = Array(10).fill(Array(10).fill(''));
+    this.board = Array.from(Array(10), () => new Array(10).fill(''));
   }
   // place ship on grid (update only)
-  // take in ship, coordinate, orientation 
+  // take in ship, coordinate, orientation (x/y)
   // return no results
   placeShip(ship, coordinate, orientation) {
-
+    const rowStart = coordinate[0];
+    const colStart = coordinate[1];
+    const rowEnd = (orientation === 'x') ? rowStart + 1 : rowStart + ship.length;
+    const colEnd = (orientation === 'y') ?  colStart + 1 : colStart + ship.length; 
+    console.log(rowStart, rowEnd, colStart, colEnd);
+    for (let row = rowStart; row < rowEnd; row++) {
+      for (let col = colStart; col < colEnd; col++) {
+        this.board[row][col] = 'O';
+      }
+    }
   }
 }
 
-const b = new Gameboard();
-console.log(b.board);
+/*
+const ships = require('./ship');
+const Ship = ships.Ship;
+const patrolBoat = new Ship('Patrol Boat', 2);
+const board = new Gameboard();
+board.placeShip(patrolBoat, [4, 9], 'y');
+console.log(board.board);
+*/
 
 module.exports = { Gameboard: Gameboard };
--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -1,8 +1,46 @@
 const gameboard = require('../src/factory/gameboard');
 const Gameboard = gameboard.Gameboard;
+const ships = require('../src/factory/ship');
+const Ship = ships.Ship;
 
-describe('test gameboard', () => {
-  test('place ship at specific coordinate', () => {
-    expect(0).toBe(1);
+describe('test gameboard ship placement', () => {
+  let board;
+  let destroyer;
+  let patrolBoat;
+  beforeEach(() => {
+    board = new Gameboard();
+    destroyer = new Ship('Destoryer', 3);
+    patrolBoat = new Ship('Patrol Boat', 2);
+  });
+  
+  test('place destroyer horizontally at (0, 0)', () => {
+    board.placeShip(destroyer, [0, 0], 'x');
+    expect(board.board).toEqual([
+      ['O', 'O', 'O', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '']
+    ]);
+  });
+  test('place patrol boat vertically at (4, 9)', () => {
+    board.placeShip(patrolBoat, [4, 9], 'y');
+    expect(board.board).toEqual([
+      ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', 'O'],
+      ['', '', '', '', '', '', '', '', '', 'O'],
+      ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '']
+    ]);
   });
 });
