29be5426fca14093aecd2843f951eceb30cb50b3
Ken Yamada <yamakenth@gmail.com>
2021-12-27 19:40:15 +0700

    Initialize game

--- src/index.js
+++ src/index.js
@@ -2,51 +2,32 @@ import './style.css';
 import { Gameboard } from './factory/gameboard';
 import { Player } from './factory/player';
 import { Ship } from './factory/ship';
-import { createContent, createHeader, createStart } from './ui-element';
+import { createRandomLayout } from './utility';
 
-// initialize new game 
-const player = new Player('Player');
-const boardP1 = new Gameboard();
-const computer = new Player('Computer');
-const boardComputer = new Gameboard();
+// initialize player 1 
+const board1 = new Gameboard();
+const ships1 = [
+  new Ship('Carrier', 5),
+  new Ship('Battleship', 4),
+  new Ship('Destroyer', 3),
+  new Ship('Submarine', 3),
+  new Ship('Patrol Boat', 2)
+]
+createRandomLayout(board1, ships1);
+const player1 = new Player('Player');
+// initialize player 2
+const board2 = new Gameboard();
+const ships2 = [
+  new Ship('Carrier', 5),
+  new Ship('Battleship', 4),
+  new Ship('Destroyer', 3),
+  new Ship('Submarine', 3),
+  new Ship('Patrol Boat', 2)
+]
+createRandomLayout(board2, ships2);
+const player2 = new Player('Player');
 
-// create header 
-createHeader();
-// populate player's board
-(function() {
-  const carrier = new Ship('Carrier', 5);
-  boardP1.placeShip(carrier, 2, 5, 'x');
-  const battleship = new Ship('Battleship', 4);
-  boardP1.placeShip(battleship, 5, 8, 'y');
-  const destroyer = new Ship('Destroyer', 3);
-  boardP1.placeShip(destroyer, 4, 4, 'y');
-  const submarine = new Ship('Submarine', 3);
-  boardP1.placeShip(submarine, 8, 2, 'x');
-  const patrolBoat = new Ship('Patrol Boat', 2);
-  boardP1.placeShip(patrolBoat, 0, 1, 'x');
-})();
-// populate computer's board
-(function() {
-  const carrier = new Ship('Carrier', 5);
-  boardComputer.placeShip(carrier, 4, 7, 'y');
-  const battleship = new Ship('Battleship', 4);
-  boardComputer.placeShip(battleship, 2, 1, 'y');
-  const destroyer = new Ship('Destroyer', 3);
-  boardComputer.placeShip(destroyer, 7, 1, 'y');
-  const submarine = new Ship('Submarine', 3);
-  boardComputer.placeShip(submarine, 4, 3, 'x');
-  const patrolBoat = new Ship('Patrol Boat', 2);
-  boardComputer.placeShip(patrolBoat, 2, 7, 'x');
-})();
-// create content 
-createContent('Player\s Turn (test)', player, computer, boardP1, boardComputer);
-// create start 
-createStart();
 
-// eventListener for attack input 
-const computerGrids = document.querySelectorAll('.board.two .col');
-computerGrids.forEach(grid => {
-  grid.addEventListener('click', () => {
-    console.log(`clicked grid (${grid.dataset.x}, ${grid.dataset.y})`);
-  });
-});
+
+console.log(board1.matrix);
+console.log(board2.matrix);
