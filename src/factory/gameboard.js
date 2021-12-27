b50e46166485642fa436af75b71f798a41bcabfe
Ken Yamada <yamakenth@gmail.com>
2021-12-27 10:47:27 +0700

    Add receiveAttack function to record hit

--- src/factory/gameboard.js
+++ src/factory/gameboard.js
@@ -20,15 +20,25 @@ class Gameboard {
     // update obj
     this.ships[ship.type] = {
       shipObj: ship,
-      position: { x, y}
+      position: { x, y }
     };
   }
 
+  // determine if an attech hit a ship, send hit(), record hit coord
+  // take in x-coord, y-coord
+  // return no results 
+  receiveAttack(x, y) {
+    const curGridItem = this.matrix[x][y];
+    if (curGridItem !== '') {
+      const curShip = this.ships[curGridItem];
+      const curShipObj = curShip.shipObj;
+      const hitPosition = (x - curShip.position.x) || (y - curShip.position.y);
+      curShipObj.hit(hitPosition);
+    }
+    this.matrix[x][y] = 'X';
+  }
 
   /*
-  // determine if a ship is hit, send hit function to the ship
-  // take in coordinate
-  // return no result 
   receiveAttack(coord) {
     const shipOnCurGrid = this.board[coord[0]][coord[1]];
     if (shipOnCurGrid !== '') {
--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -75,7 +75,7 @@ describe('test gameboard ship placement', () => {
   });
 });
 
-describe.skip('test receiveAttack', () => {
+describe('test receiveAttack', () => {
   let board;
   let destroyer;
   let patrolBoat;
@@ -83,38 +83,46 @@ describe.skip('test receiveAttack', () => {
     board = new Gameboard();
     destroyer = new Ship('Destroyer', 3);
     patrolBoat = new Ship('Patrol Boat', 2);
+    board.placeShip(destroyer, 0, 0, 'x');
+    board.placeShip(patrolBoat, 4, 9, 'y');
   });
 
-  test('hit a destroyer placed at (2, 3) vertically', () => {
-    board.placeShip(destroyer, [2, 3], 'y');
-    board.receiveAttack([3, 3]);
-    expect(board.board).toEqual([
+  test('matrix after an attack hit a ship', () => {
+    board.receiveAttack(0, 1);
+    expect(board.matrix).toEqual([
+      ['Destroyer', 'X', 'Destroyer', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', '', '', ''],
-      ['', '', '', 'Destroyer', '', '', '', '', '', ''],
-      ['', '', '', 'X', '', '', '', '', '', ''],
-      ['', '', '', 'Destroyer', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', 'Patrol Boat'],
+      ['', '', '', '', '', '', '', '', '', 'Patrol Boat'],
       ['', '', '', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', '', '', '']
     ]);
   });
-  test('no hit', () => {
-    board.placeShip(destroyer, [2, 3], 'y');
-    board.receiveAttack([0, 1]);
-    expect(board.board).toEqual([
-      ['', 'X', '', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', '', '', ''],
-      ['', '', '', 'Destroyer', '', '', '', '', '', ''],
-      ['', '', '', 'Destroyer', '', '', '', '', '', ''],
-      ['', '', '', 'Destroyer', '', '', '', '', '', ''],
+  test('matrix after an attack miss a ship', () => {
+    board.receiveAttack(9, 1);
+    expect(board.matrix).toEqual([
+      ['Destroyer', 'Destroyer', 'Destroyer', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', 'Patrol Boat'],
+      ['', '', '', '', '', '', '', '', '', 'Patrol Boat'],
       ['', '', '', '', '', '', '', '', '', ''],
-      ['', '', '', '', '', '', '', '', '', '']
+      ['', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', ''],
+      ['', 'X', '', '', '', '', '', '', '', '']
     ]);
+  });
+  test('a ship after an attack hit a ship', () => {
+    board.receiveAttack(0, 1);
+    expect(destroyer.positions).toEqual([0, 1, 0]);
+  });
+  test('a ship after an attack hit a ship 2', () => {
+    board.receiveAttack(5, 9);
+    expect(patrolBoat.positions).toEqual([0, 1]);
   });
 });
