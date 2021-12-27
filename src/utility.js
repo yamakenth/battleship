68edbfba1a3579236888022f571b1e938e37f19d
Ken Yamada <yamakenth@gmail.com>
2021-12-27 19:35:16 +0700

    Add function to generate random layout

--- src/utility.js
+++ src/utility.js
@@ -8,4 +8,20 @@ function generateRandCoord() {
   }
 }
 
-export { generateRandCoord };
+// place ship on baord randomly 
+// take in board, array of ships 
+// return no results 
+function createRandomLayout(board, ships) {
+  let i = 0;
+  while (i < ships.length) {
+    const { x, y } = generateRandCoord();
+    const orientation = (Math.floor(Math.random() * 2) === 0) ? 'x' : 'y';
+    const placeShipSuccess = board.placeShip(ships[i], x, y, orientation);
+    if (placeShipSuccess === -1) {
+      continue;
+    }
+    i++;
+  }
+}
+
+export { generateRandCoord, createRandomLayout };
