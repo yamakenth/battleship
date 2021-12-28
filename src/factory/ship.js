3b670f43d73e7c9aca26bd8cdb762978a9ace786
Ken Yamada <yamakenth@gmail.com>
2021-12-28 12:53:33 +0700

    Add whitespace for readability

--- src/factory/ship.js
+++ src/factory/ship.js
@@ -6,18 +6,21 @@ class Ship {
     this.positions = Array(length).fill(0);
     this.headIndex = {};
   }
+
   // set first index of ship 
   // take in x, y 
   // return no result 
   setHeadIndex(x, y) {
     this.headIndex = { x, y };
   }
+
   // modify positions array when hit 
   // take in hit position 
   // return no results 
   hit(position) {
     this.positions[position] = 1;
   }
+  
   // determine if ship is sunk
   // take in no parameters 
   // return boolean 
